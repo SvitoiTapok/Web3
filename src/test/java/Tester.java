0ef524d3715dcf53860ad72555a318de2a0d4e5d
Alexey Pavlov <pavel.alex.030405@gmail.com>
2025-04-22 12:02:36 +0300

    test tests

---
+++ src/test/java/Tester.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import com.example.lab3.util.RequestParser;
+import com.google.common.annotations.VisibleForTesting;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tester {
+    @Test
+    public void testOne(){
+        assertTrue(RequestParser.hitCheck(0,0,2));
+    }
+    @Test
+    public void testTwo(){
+        assertFalse(RequestParser.hitCheck(-1,1,3));
+    }
+    @Test
+    public void testThree(){
+        assertTrue(RequestParser.hitCheck(-1,-1,5));
+    }
+
+
+
+}
